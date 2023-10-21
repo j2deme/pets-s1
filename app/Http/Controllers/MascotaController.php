2df3cbc27c608f74135fd11cfb8fcf28c66aabf8
j2deme <j2deme@gmail.com>
2023-10-21 08:45:27 -0600

    ♻️ Consulta y visualiza mascotas en controlador

--- app/Http/Controllers/MascotaController.php
+++ app/Http/Controllers/MascotaController.php
@@ -12,7 +12,8 @@ class MascotaController extends Controller
      */
     public function index()
     {
-        //
+        $mascotas = Mascota::all();
+        return view("mascotas.index", compact("mascotas"));
     }
 
     /**
---
+++ resources/views/mascotas/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    {{ $mascotas }}
+</div>
