f7b3a92aacbf26b7d8a8d23fcdda186c9a22d77d
j2deme <j2deme@gmail.com>
2023-10-24 19:37:18 -0600

    💄 Agrega layout principal

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Mascotas</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+  <header>
+    <h1>Mascotas</h1>
+  </header>
+  <main>
+    @yield('contenido')
+  </main>
+</body>
+
+</html>
